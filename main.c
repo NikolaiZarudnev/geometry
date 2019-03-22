746bf1ddbbef468626f4d26df5df428789a4f89d
NikolaiZarudnev <nikolaizarudnrv@gmail.com>
2019-03-23 03:29:15 +0700

    added cross_circle

--- main.c
+++ main.c
@@ -1,32 +1,52 @@
 #include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
 #define M_PI 3.14
 
 typedef struct {
 	float x, y;
 	float r;
-}circle;
+}Circle;
 
-circle circle_input(circle shape)
+Circle circle_input()
 {
+	Circle shape;
 	printf("Enter x and y:\n");
 	scanf("%f %f", &shape.x, &shape.y);
 	printf("Enter radius\n");
 	scanf("%f", &shape.r);
-
 	return shape;
 }
 
-void description_circle(circle shape)
+int cross_circle(Circle shape1, Circle shape2)
 {
-	printf("perimeter: %.2f\n", (2.0 * M_PI * shape.r));
-	printf("area: %.2f\n", (M_PI * shape.r * shape.r));
+	if ((sqrt(pow((shape1.x - shape2.x), 2) + pow((shape1.y - shape2.y), 2)) < shape1.r + shape2.r) && 
+		(sqrt(pow((shape1.x - shape2.x), 2) + pow((shape1.y - shape2.y), 2)) > abs(shape1.r - shape2.r))) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+void description_circle(Circle shape1, Circle shape2)   
+{
+	printf("perimeter: %.2f\n", (2.0 * M_PI * shape1.r));
+	printf("area: %.2f\n", (M_PI * shape1.r * shape1.r));
+
+	if(cross_circle(shape1, shape2)) {
+		printf("intersects:\n");
+		printf("  \n");                                         //оформление вывода???
+	} else {
+		printf("no intersects\n");
+	}
 }
 
 int main()
 {
-	circle a;
-	
-	a = circle_input(a);
-	description_circle(a);
+	Circle a, b;
+	a = circle_input();
+	b = circle_input();
+	description_circle(a, b);
 	return 0;
 }
