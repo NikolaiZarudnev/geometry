bb13199ed8e2e3c5392f6c7c27ce4bc61d405d7f
NikolaiZarudnev <nikolaizarudnrv@gmail.com>
2019-03-23 12:53:07 +0700

    update input

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ typedef struct {
 	float r;
 }Circle;
 
- float string_to_float(char c[])
+ float string_to_float(char* c, int x)
  {
 	int i;
 	float n,v;
@@ -24,9 +24,9 @@ typedef struct {
 	if (c[i] != '.') {
 		return n;
 	}
-	for (i++, v = 0.1; c[i] >= '0' && c[i] <= '9'; i++){
-		n += v * (c[i] - '0'); 
-		v = v / 10;                               
+	for (i++, v = 0.1; c[i] >= '0' && c[i] <= '9'; i++) {
+		n += v * (c[i] - '0');
+		v = v / 10;                      
 	}
 	return n;
 }
@@ -34,13 +34,19 @@ typedef struct {
 Circle circle_input()
 {
 	Circle shape;
-	char stringNAME[20];
-	float shapexyr[3];
+	char stringNAME[25];
+	float circlexyr[3];
 	printf("enter circle(x, y, radius)\n");
-	fgets(stringNAME, 20, stdin);
+	fgets(stringNAME, 25, stdin);
 	if (!strcmp(stringNAME, "circle(%f, %f, %f")) {                     //входные данные???
 		printf("error\n");
 	}
+	for (int i = 0; i < 3; ++i) {
+		circlexyr[i] = string_to_float(stringNAME, 25);
+	}
+	shape.x = circlexyr[0];
+	shape.y = circlexyr[1];
+	shape.r = circlexyr[2];
 	return shape;
 }
 
@@ -59,7 +65,7 @@ void description_circle(Circle shape1, Circle shape2)
 	printf("area: %.2f\n", (M_PI * shape1.r * shape1.r));
 
 	if(cross_circle(shape1, shape2)) {
-		printf("intersects:\n");
+		printf("intersects with 2. circle\n");
 		printf("  \n");                                         //оформление вывода???
 	}else{
 		printf("no intersects\n");
