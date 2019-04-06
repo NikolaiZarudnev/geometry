32574bf3601235dab52469db7f52eb8861d3fcba
NikolaiZarudnev <nikolaizarudnrv@gmail.com>
2019-04-06 12:18:36 +0700

    Splited from main to description_shape.c

---
+++ src/description_shape.h
@@ -0,0 +1,13 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    float x, y;
+    float r;
+} Circle;
+
+int cross_circle(Circle shape1, Circle shape2)
+
+void description_circle(Circle shape1, Circle shape2)
