ac44dbdd7b65890afdb5c7b63ab96556b26669b8
NikolaiZarudnev <nikolaizarudnrv@gmail.com>
2019-04-06 12:20:55 +0700

    main.c

---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "input_output.h"
+#include "description_shape.h"
+
+int main()
+{
+    Circle a, b;
+    a = circle_input();
+    b = circle_input();
+    description_circle(a, b);
+    return 0;
+}
